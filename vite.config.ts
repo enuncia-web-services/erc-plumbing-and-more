8a581077da35ef87a52cbb26d88346d8fe49fa81
Manoharan Nair <your_github_email_here>
2025-11-24 21:04:19 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,27 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      base: '/erc-plumbing-and-more/',
+  const env = loadEnv(mode, '.', '');
 
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
+  return {
+    base: '/erc-plumbing-and-more/',
+
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+    },
+
+    plugins: [react()],
+
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'), // root folder
       },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
